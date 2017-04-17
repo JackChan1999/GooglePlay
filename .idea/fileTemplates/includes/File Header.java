3389e113e7c515784c79713e65fac29e157c9b05
JackChen1999 <jackychan2040@gmail.com>
2017-04-17 22:12:51 +0800

    ignore file

---
+++ .idea/fileTemplates/includes/File Header.java
@@ -0,0 +1,20 @@
+/**
+ * ============================================================
+ * Copyright：${TODO}有限公司版权所有 (c) ${YEAR}
+ * Author：   陈冠杰
+ * Email：    815712739@qq.com
+ * GitHub：   https://github.com/JackChen1999
+ * 博客：     http://blog.csdn.net/axi295309066
+ * 微博：     AndroidDeveloper
+ *
+ * Project_Name：${PROJECT_NAME}
+ * Package_Name：${PACKAGE_NAME}
+ * Version：1.0
+ * time：${DATE} ${TIME}
+ * des ：${TODO}
+ * gitVersion：$Rev$
+ * updateAuthor：$Author$
+ * updateDate：$Date$
+ * updateDes：${TODO}
+ * ============================================================
+ **/
